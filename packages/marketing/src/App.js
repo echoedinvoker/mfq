62f1fd354e0953fa76d2aac843d38ac2b03f874d
Matt Chang <echoedinvoker@gmail.com>
2024-07-04 16:29:21 +0800

    fix typo error

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,16 +1,16 @@
 import React from "react"
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles"
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Router } from "react-router-dom"
 import Landing from "./components/Landing"
 import Pricing from "./components/Pricing"
 
 export default ({ history }) => {
   return <StylesProvider generateClassName={createGenerateClassName({ productionPrefix: "ma" })}>
-    <Route history={history}>
+    <Router history={history}>
       <Switch>
         <Route exact path="/pricing" component={Pricing} />
         <Route path="/" component={Landing} />
       </Switch>
-    </Route>
+    </Router>
   </StylesProvider>
 }
