1b0f29c38eef9e6910c6d58eafd6b2a9c262c092
Matt Chang <echoedinvoker@gmail.com>
2024-07-04 12:05:18 +0800

    demo

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            Apppp
           </Typography>
           <Button
             color="primary"
--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Home Pagea
             </Typography>
             <Typography
               variant="h5"
