dcdeddab4e34eb0e025aa0a2772480ab90aa4260
Matt Chang <echoedinvoker@gmail.com>
2024-07-04 09:26:21 +0800

    demo

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            Appp
+            App
           </Typography>
           <Button
             color="primary"
