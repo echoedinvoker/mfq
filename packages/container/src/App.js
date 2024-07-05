f8806e679c56bc9896a9f645e5f4bd420d54adab
Matt Chang <echoedinvoker@gmail.com>
2024-07-05 19:26:26 +0800

    104- adding initial state to memory history

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp'
+import AuthApp from './components/AuthApp'
 import Header from './components/Header'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 export default () => {
   return (
@@ -10,7 +11,10 @@ export default () => {
       <BrowserRouter>
         <div>
           <Header />
-          <MarketingApp />
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
         </div>
       </BrowserRouter>
     </StylesProvider>
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -4,8 +4,10 @@ import App from './App';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath]
+  });
 
   if (onNavigate) {
     history.listen(onNavigate);
