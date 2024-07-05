6b87e72f9d7867502a926e1495586f0cbb7f4c57
Matt Chang <echoedinvoker@gmail.com>
2024-07-06 02:50:54 +0800

    106- adding a loading bar

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
-import MarketingApp from './components/MarketingApp'
-import AuthApp from './components/AuthApp'
 import Header from './components/Header'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Progress from './components/Progress'
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'))
+const AuthLazy = lazy(() => import('./components/AuthApp'))
 
 export default () => {
   return (
@@ -11,10 +13,12 @@ export default () => {
       <BrowserRouter>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
