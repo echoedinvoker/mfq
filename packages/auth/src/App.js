4ad5e5acaec4322c8be31fb2d6c90e0dcbba839e
Matt Chang <echoedinvoker@gmail.com>
2024-07-06 09:16:46 +0800

    109- communicating auth changes

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -5,12 +5,18 @@ import { Switch, Route, Router } from "react-router-dom"
 import Signin from "./components/Signin"
 import Signup from "./components/Signup"
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
   return <StylesProvider generateClassName={createGenerateClassName({ productionPrefix: "au" })}>
     <Router history={history}>
       <Switch>
-        <Route path="/auth/signin" component={Signin} />
-        <Route path="/auth/signup" component={Signup} />
+        {/* <Route path="/auth/signin" component={Signin} /> */}
+        {/* <Route path="/auth/signup" component={Signup} /> */}
+        <Route path="/auth/signin">
+          <Signin onSignIn={onSignIn} />
+        </Route>
+        <Route path="/auth/signup">
+          <Signup onSignIn={onSignIn} />
+        </Route>
       </Switch>
     </Router>
   </StylesProvider>
